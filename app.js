f432e65ddbe334082484a1e8b896e9178d94468e
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2023-07-17 02:21:22 +0530

    added particles, fixed issue with redering

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import t1 from "./img/t1.png";
+import t2 from "./img/t2.png";
 import fragment from "./shaders/fragment.glsl";
 import vertex from "./shaders/vertex.glsl";
 
@@ -9,15 +12,21 @@ export default class Sketch {
     document.getElementById("container").appendChild(this.renderer.domElement);
 
     this.camera = new THREE.PerspectiveCamera(
-      45,
+      70,
       window.innerWidth / window.innerHeight,
-      1,
+      0.1,
       3000
     );
     this.camera.position.z = 1000;
     this.scene = new THREE.Scene();
-    this.addMesh();
+
+    this.textures = [
+      new THREE.TextureLoader().load(t1),
+      new THREE.TextureLoader().load(t2),
+    ];
     this.time = 0;
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.addMesh();
     this.render();
   }
 
@@ -27,25 +36,33 @@ export default class Sketch {
       vertexShader: vertex,
       uniforms: {
         progress: { type: "f", value: 0 },
+        t1: { type: "t", value: this.textures[0] },
+        t2: { type: "t", value: this.textures[1] },
       },
       side: THREE.DoubleSide,
     });
 
-    this.geometry = new THREE.BufferGeometry();
     let number = 512 * 512;
-    this.positions = new THREE.BufferAttribute(new Float32Array(number * 3));
+    this.geometry = new THREE.BufferGeometry();
+
+    this.positions = new THREE.BufferAttribute(new Float32Array(number * 3), 3);
+    this.coordinates = new THREE.BufferAttribute(
+      new Float32Array(number * 3),
+      3
+    );
 
     let index = 0;
     for (let i = 0; i < 512; i++) {
+      let posX = i - 256;
       for (let j = 0; j < 512; j++) {
-        this.positions.setXYZ(index, i, j, 0);
+        this.positions.setXYZ(index, posX * 2, (j - 256) * 2, 0);
+        this.coordinates.setXYZ(index, i, j, 0);
         index++;
       }
     }
 
-    this.geometry.setAttribute("postion", this.positions);
-
-    console.log(this.positions);
+    this.geometry.setAttribute("position", this.positions);
+    this.geometry.setAttribute("aCoordinates", this.coordinates);
 
     this.mesh = new THREE.Points(this.geometry, this.material);
     this.scene.add(this.mesh);
@@ -53,8 +70,8 @@ export default class Sketch {
 
   render() {
     this.time++;
-    this.mesh.rotation.x += 0.01;
-    this.mesh.rotation.y += 0.02;
+    // this.mesh.rotation.x += 0.01;
+    // this.mesh.rotation.y += 0.02;
     this.renderer.render(this.scene, this.camera);
     window.requestAnimationFrame(this.render.bind(this));
   }
